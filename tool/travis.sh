36df04d92f0ebb1bb9b473a65049c3253fa2a72f
Kevin Moore <kevmoo@google.com>
2019-07-21 21:40:03 -0700

    Support the latest pkg:build_web_compilers

--- tool/travis.sh
+++ tool/travis.sh
@@ -46,13 +46,13 @@ while (( "$#" )); do
     ;;
   dartdevc_test) echo
     echo -e '\033[1mTASK: dartdevc_test\033[22m'
-    echo -e 'pub run build_runner test -- -P travis -p chrome'
-    pub run build_runner test -- -p chrome || EXIT_CODE=$?
+    echo -e 'xvfb-run pub run build_runner test -- -P travis -p chrome'
+    xvfb-run pub run build_runner test -- -p chrome || EXIT_CODE=$?
     ;;
   dart2js_test) echo
     echo -e '\033[1mTASK: dart2js_test\033[22m'
-    echo -e 'pub run test -P travis -p chrome -x requires-dart2'
-    pub run test -p chrome || EXIT_CODE=$?
+    echo -e 'xvfb-run pub run test -P travis -p chrome -x requires-dart2'
+    xvfb-run pub run test -p chrome || EXIT_CODE=$?
     ;;
   *) echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
     EXIT_CODE=1
